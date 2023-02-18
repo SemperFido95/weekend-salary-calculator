0d6e83a50bf276fb7b986c3cf0d3cc3114c0079c
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-02-18 15:27:14 -0500

    completed base mode requirements

--- client.js
+++ client.js
@@ -4,7 +4,6 @@ let salaries = [];
 
 let submitForm = event => {
     event.preventDefault();
-    console.log('Testing submitForm()', event);
     salaries.push({
         firstName: event.target[0].value,
         lastName: event.target[1].value,
@@ -28,7 +27,6 @@ let clearInputs = () => {
 let addToTable = array => {
     let tableDiv = document.getElementById('add-new');
     let newEmployee = array[array.length - 1];
-    console.log('new employe object:', newEmployee)
     tableDiv.innerHTML += `
         <tr>
             <td>${newEmployee.firstName}</td>
