c01f2324e66c37e25c240e8a59ba68323c5a3ef5
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-02-18 15:01:53 -0500

    created addToTable() function

--- client.js
+++ client.js
@@ -5,17 +5,15 @@ let salaries = [];
 let submitForm = event => {
     event.preventDefault();
     console.log('Testing submitForm()', event);
-
     salaries.push({
         firstName: event.target[0].value,
         lastName: event.target[1].value,
         idNumber: event.target[2].valueAsNumber,
         job: event.target[3].value,
         annualSalary: event.target[4].valueAsNumber
-    })
-
+    });
     clearInputs();
-    console.log(salaries)
+    addToTable(salaries);
 }
 
 let clearInputs = () => {
@@ -24,4 +22,19 @@ let clearInputs = () => {
     for (let i = 0; i < children.length - 1; i++) {
         children[i].value = '';
     }
+}
+
+let addToTable = array => {
+    let tableDiv = document.getElementById('add-new');
+    let newEmployee = array[array.length - 1];
+    console.log('new employe object:', newEmployee)
+    tableDiv.innerHTML += `
+        <tr>
+            <td>${newEmployee.firstName}</td>
+            <td>${newEmployee.lastName}</td>
+            <td>${newEmployee.idNumber}</td>
+            <td>${newEmployee.job}</td>
+            <td>${newEmployee.annualSalary}</td>
+        </tr>
+    `
 }
