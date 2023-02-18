4a2952c38e3f0dc22efe10d0cb66840e6ce5b201
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-02-18 14:21:19 -0500

    created salaries array and added salaries.push() inside of submitForm()

--- client.js
+++ client.js
@@ -1,6 +1,18 @@
 console.log('Hello World')
 
+let salaries = [];
+
 let submitForm = event => {
     event.preventDefault();
-    console.log('Testing submitForm()')
+    console.log('Testing submitForm()', event);
+
+    salaries.push({
+        firstName: event.target[0].value,
+        lastName: event.target[1].value,
+        idNumber: event.target[2].valueAsNumber,
+        job: event.target[3].value,
+        annualSalary: event.target[4].valueAsNumber
+    })
+    console.log(salaries)
 }
+
